ffb7915760aadaaa75f69989d73fbe72afe3b242
Neelakshi Choudhary <113431425+neelakshi2511@users.noreply.github.com>
2025-11-04 11:03:47 +0530

    fixed addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,6 +1,6 @@
 def add_two_nums(num1 , num2):
   return num1 + num2 
 def test_add_two_nums():
-  assert add(1 , 2) == 3
-  assert add(1 , -1) == 0
-  assert add(5 , 10) == 15
+  assert add_two_nums(1 , 2) == 3
+  assert add_two_nums(1 , -1) == 0
+  assert add_two_nums(5 , 10) == 15
